ba541223f67726e73dbe128554b1b2fe97f183ed
Guetti <61527120+Guetti@users.noreply.github.com>
2022-09-06 12:27:15 -0300

    feat(GUI.py): add linking and unlinking buttons

---
+++ GUI.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter.ttk import Button
+from Constants import APP_TITLE
+# Declare the window.
+window = tk.Tk()
+
+# Lock the size of the window.
+window.resizable(width=False, height=False)
+
+# Set the window title
+window.title(APP_TITLE)
+
+# Declare the contacting button.
+contacting_button = Button(text="Leer archivo de contrataciones")
+
+# Declare the unlinking button.
+unlinking_button = Button(text="Desvincular trabajador")
+
+# Add the buttons to the grid.
+contacting_button.grid(row=2, column=2, sticky="W", pady=2)
+unlinking_button.grid(row=2, column=4, sticky="E", pady=2)
+
+# Run the main window.
+window.mainloop()
