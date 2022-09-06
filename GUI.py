cb4af012eade37ab5b010e266034d03bed8658a3
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2022-09-06 20:54:04 -0300

    feat(GUI.py): add generate report button, link buttons with RRHH_impl.py methods.

--- GUI.py
+++ GUI.py
@@ -1,6 +1,11 @@
 import tkinter as tk
-from tkinter.ttk import Button
-from resources.Constants import APP_TITLE
+from tkinter.ttk import Button, Style
+from resources.Constants import (
+    APP_TITLE)
+from Requirements.RRHH_impl import (
+    read_linking_file,
+    read_unlinking_file,
+    generate_report)
 
 
 def show_window():
@@ -14,15 +19,25 @@ def show_window():
     # Set the window title
     window.title(APP_TITLE)
 
-    # Declare the contacting button.
-    contacting_button = Button(text="Leer archivo de contrataciones")
+    # Declare the read linking file button.
+    linking_button = Button(window,
+                            text="Leer archivo\nde contrataciones",
+                            command=read_linking_file)
 
-    # Declare the unlinking button.
-    unlinking_button = Button(text="Desvincular trabajador")
+    # Declare the read unlinking file button.
+    unlinking_button = Button(window,
+                              text="Leer archivo\nde desvinculaciones",
+                              command=read_unlinking_file)
+
+    # Declare the export report button.
+    generate_report_button = Button(window,
+                                    text="Generar reporte de pana",
+                                    command=generate_report)
 
     # Add the buttons to the grid.
-    contacting_button.grid(row=2, column=2, sticky="W", pady=2)
-    unlinking_button.grid(row=2, column=4, sticky="E", pady=2)
+    linking_button.grid(row=0, column=0, sticky="W", pady=2)
+    unlinking_button.grid(row=0, column=2, sticky="E", pady=2)
+    generate_report_button.grid(row=1, column=1, sticky="W", pady=2)
 
     # Run the main window.
     window.mainloop()
