1e8ce711ae0fe5ed2d973aa9f7cfc5e5bfc7fcf1
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2022-09-06 18:03:36 -0300

    feat(Requirements): add implementation package

---
+++ Requirements/RRHH_impl.py
@@ -0,0 +1,41 @@
+"""el joeloop trabaja de pana acá
+haga lo que estime conveniente con los tipos de datos
+y cualquier weaita que se le ocurra
+todo lo que está escrito es una recomendación y debe ser tratada como tal
+"""
+
+
+#region Requerimiento 2
+
+def add_employee():
+    pass
+
+def edit_employee():
+    pass
+
+def add_contract(rut: str):
+    pass
+
+def edit_contract(rut: str):
+    pass
+
+#endregion
+
+#region Requerimiento 3
+
+def read_linking_file():
+    print("pasa")
+    pass
+
+def read_unlinking_file():
+    print("te traspasa")
+    pass
+
+#endregion
+
+#region Requerimiento 4
+
+def generate_report():
+    pass
+
+#endregion
