70237e3481cbe844c2f061c184ab7bc2c28a8a5d
Guetti <61527120+Guetti@users.noreply.github.com>
2022-09-13 21:36:34 -0300

    style(update): change text jump space

--- resources/Constants.py
+++ resources/Constants.py
@@ -1,4 +1,4 @@
 APP_TITLE = "Recursos Humanos"
 
-linking_button_txt = "Leer archivo\nde contrataciones"
-unlinking_button_txt = "Leer archivo\nde desvinculaciones"
+linking_button_txt = "Leer archivo de contrataciones"
+unlinking_button_txt = "Leer archivo de desvinculaciones"
