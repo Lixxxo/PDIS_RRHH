0ce3e4c4c30948604f01be1bc78519d9d8f9b98f
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2022-09-14 18:56:43 -0300

    feat: add constructor declaration

--- Logic/RRHH_System.py
+++ Logic/RRHH_System.py
@@ -1,6 +1,9 @@
 
 class RRHHSystem:
+
     employees = []
     contracts = []
 
-
+    def __init__(self, employees, contracts):
+        self.employees = employees
+        self.contracts = contracts
